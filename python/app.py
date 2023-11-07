2398c861166b0beb8ca5a5de3ef82754c23436cf
Moke Zhang <17319370515@163.com>
2023-11-07 17:26:15 +0800

    Update to support python3

--- python/app.py
+++ python/app.py
@@ -1,7 +1,9 @@
 import base64
-import urlparse
-from tlslite.utils import keyfactory
+from urllib.parse import parse_qsl
+
 import oauth2 as oauth
+from tlslite.utils import keyfactory
+
 
 class SignatureMethod_RSA_SHA1(oauth.SignatureMethod):
     name = 'RSA-SHA1'
@@ -31,21 +33,19 @@ class SignatureMethod_RSA_SHA1(oauth.SignatureMethod):
         privateKeyString = data.strip()
 
         privatekey = keyfactory.parsePrivateKey(privateKeyString)
-        signature = privatekey.hashAndSign(raw)
+        signature = privatekey.hashAndSign(raw.encode())
 
         return base64.b64encode(signature)
-
-
 
 
 consumer_key = 'oauth-sample-consumer'
 consumer_secret = 'dont_care'
 
-request_token_url = 'http://localhost:8090/jira/plugins/servlet/oauth/request-token'
-access_token_url = 'http://localhost:8090/jira/plugins/servlet/oauth/access-token'
-authorize_url = 'http://localhost:8090/jira/plugins/servlet/oauth/authorize'
+request_token_url = 'http://localhost:7080/plugins/servlet/oauth/request-token'
+access_token_url = 'http://localhost:7080/plugins/servlet/oauth/access-token'
+authorize_url = 'http://localhost:7080/plugins/servlet/oauth/authorize'
 
-data_url = 'http://localhost:8090/jira/rest/api/2/issue/BULK-1'
+data_url = 'http://localhost:7080/rest/api/2/issue/BSD-1'
 
 consumer = oauth.Consumer(consumer_key, consumer_secret)
 client = oauth.Client(consumer)
@@ -65,29 +65,29 @@ client.set_signature_method(SignatureMethod_RSA_SHA1())
 
 resp, content = client.request(request_token_url, "POST")
 if resp['status'] != '200':
-    raise Exception("Invalid response %s: %s" % (resp['status'],  content))
+    raise Exception("Invalid response %s: %s" % (resp['status'], content))
 
-request_token = dict(urlparse.parse_qsl(content))
+request_token = dict(parse_qsl(content.decode()))
 
-print "Request Token:"
-print "    - oauth_token        = %s" % request_token['oauth_token']
-print "    - oauth_token_secret = %s" % request_token['oauth_token_secret']
-print
+print("Request Token:")
+print("    - oauth_token        = %s" % request_token['oauth_token'])
+print("    - oauth_token_secret = %s" % request_token['oauth_token_secret'])
+print()
 
 # Step 2: Redirect to the provider. Since this is a CLI script we do not
 # redirect. In a web application you would redirect the user to the URL
 # below.
 
-print "Go to the following link in your browser:"
-print "%s?oauth_token=%s" % (authorize_url, request_token['oauth_token'])
-print
+print("Go to the following link in your browser:")
+print("%s?oauth_token=%s" % (authorize_url, request_token['oauth_token']))
+print()
 
 # After the user has granted access to you, the consumer, the provider will
 # redirect you to whatever URL you have told them to redirect to. You can
 # usually define this in the oauth_callback argument as well.
 accepted = 'n'
 while accepted.lower() == 'n':
-    accepted = raw_input('Have you authorized me? (y/n) ')
+    accepted = input('Have you authorized me? (y/n) ')
 # oauth_verifier = raw_input('What is the PIN? ')
 
 # Step 3: Once the consumer has redirected the user back to the oauth_callback
@@ -96,21 +96,20 @@ while accepted.lower() == 'n':
 # request token and use the access token returned. You should store this
 # access token somewhere safe, like a database, for future use.
 token = oauth.Token(request_token['oauth_token'],
-    request_token['oauth_token_secret'])
-#token.set_verifier(oauth_verifier)
+                    request_token['oauth_token_secret'])
+# token.set_verifier(oauth_verifier)
 client = oauth.Client(consumer, token)
 client.set_signature_method(SignatureMethod_RSA_SHA1())
 
 resp, content = client.request(access_token_url, "POST")
-access_token = dict(urlparse.parse_qsl(content))
+access_token = dict(parse_qsl(content.decode()))
 
-print "Access Token:"
-print "    - oauth_token        = %s" % access_token['oauth_token']
-print "    - oauth_token_secret = %s" % access_token['oauth_token_secret']
-print
-print "You may now access protected resources using the access tokens above."
-print
-
+print("Access Token:")
+print("    - oauth_token        = %s" % access_token['oauth_token'])
+print("    - oauth_token_secret = %s" % access_token['oauth_token_secret'])
+print()
+print("You may now access protected resources using the access tokens above.")
+print()
 
 # Now lets try to access the same issue again with the access token. We should get a 200!
 accessToken = oauth.Token(access_token['oauth_token'], access_token['oauth_token_secret'])
